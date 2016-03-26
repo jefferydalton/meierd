9197c2700e68dd99fdbdd09e0375f8abb39b9922
Jeff Dalton <jeff@simpleaspossible.xyz>
2016-03-26 19:21:13 -0400

    Connected to html page and counted number of H2 sections

---
+++ src/fs/quoteExtract/parseQuotes.fs
@@ -0,0 +1,12 @@
+﻿module parseQuotes
+
+open FSharp.Data
+open System
+open System.IO
+
+let quoteUrl = @"http://sourcesofinsight.com/inspirational-quotes/"
+
+let url = "http://sourcesofinsight.com/inspirational-quotes/"
+let pageHtml = 
+    HtmlDocument.Load(url).Descendents("h2") |> Seq.head
+

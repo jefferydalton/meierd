aad8d88fdbd413841b18525b56c6121fe223face
Jeff Dalton <jeff@simpleaspossible.xyz>
2016-03-28 23:46:22 -0400

    Added routines to get h2 and p tags.  Added Category routine

--- src/fs/MeiredQuotes/parseQuotes.fs
+++ src/fs/MeiredQuotes/parseQuotes.fs
@@ -2,18 +2,23 @@
 
 open FSharp.Data
 
-
-
 type Quote = {Category:string; Author:string; QuoteText:string;}
+
+let private getPageHeadersAndParagraphs(url:string) =
+    Seq.toList(HtmlDocument.Load(url).Descendants(["h2"; "p"]))
+
+
 let getQuotes(url:string) =
-    let pageHtml = 
-        HtmlDocument.Load(url)
-
+    let rec processCategory (acc:Quote list) (items:HtmlNode list) =
+        match items with
+        | head::tail ->
+            if head.Name() = "h2" then
+                processCategory ({ Category=head.InnerText(); Author="Dummy"; QuoteText="Dummy"; } :: acc) tail
+            else
+                processCategory acc tail 
+        | [] -> acc
+    let x = 
+        getPageHeadersAndParagraphs(url) |> processCategory [] 
+  
+     
     [{ Category="Achievement"; Author="Adam Ant"; QuoteText="Achievement results from work realizing ambition"}]
-
-
-//let private pageHtml = 
-//    HtmlDocument.Load(quoteUrl).Descendants("h2") |> Seq.head
-
-
-

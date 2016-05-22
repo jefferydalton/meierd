df4a1c180e5088fda9afb2a2d3cc2391b2a69cb9
jeffdalton <jeff.dalton@volvo.com>
2016-05-22 17:48:29 -0400

    API Service and MS Band Package Version 1

---
+++ src/cs/QuoteService/src/QuoteService/Controllers/QuotesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QuoteService.View;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace QuoteService.Controllers
+{
+    [Route("api/[controller]")]
+    public class QuotesController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public QuoteView Get()
+        {
+            return new QuoteView { Quote = "“Courage doesn’t always roar. Sometimes courage is the quiet voice at the end of the day saying, I will try again tomorrow.” — Mary Anne Radmacher" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public QuoteView Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
---
+++ src/cs/QuoteService/src/QuoteService/View/QuoteView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuoteService.View
+{
+    public class QuoteView
+    {
+        public string Quote { get; set; }
+    }
+}
